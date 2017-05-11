2e221503199ded9a125da9a907ff3000fc850101
Stevenzwzhai <zzwdzsw@sina.cn>
2017-05-11 15:52:37 +0800

    'done'

---
+++ client.js
@@ -0,0 +1,70 @@
+const net = require('net')
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+//建立客户端
+//设定发送消息固定格式（协议）
+const send = {
+    name:"",
+    message:"",
+    to:""
+}
+
+rl.question('What is your name >', (name) => {
+    if(name){
+        const client = net.connect({
+            port:4433,
+            host:'172.20.131.140'
+        }, (data) => {
+            console.log('welcome to chatroom 4433!');
+            
+            client.on('data', (data) => {
+                //这里data是一个buffer;
+                let buf = new Buffer('');
+                readline.clearLine(buf,-1);
+                let receive = JSON.parse(data.toString().trim());
+                process.stdout.write(`\n${receive.name}>${receive.message}\n`);
+                rl.prompt();
+            }).on('error', (err) => {
+                console.log('\n服务器断了稍后连接！')
+                process.exit(0);
+            })
+
+            process.stdin.on('data', (chunck) => {
+
+            })  
+
+            send.name = name.trim();
+            send.to = "login";
+            send.message = "上线了";
+            client.write(JSON.stringify(send));
+            rl.setPrompt(`${name.trim()}>`);
+            rl.prompt();
+            rl.on('line', (line) => {
+                let stdinInfo = line.trim().split(':');
+                if(stdinInfo.length == 2){
+                    send.to = stdinInfo[0];
+                    send.message = stdinInfo[1];
+                }else{
+                    send.to = "server";
+                    send.message = stdinInfo[0];
+                }
+                client.write(JSON.stringify(send));
+                rl.prompt();
+            }).on('close', () => {
+                console.log('outline');
+                process.exit(0);
+            });   
+        })
+        
+        
+    client.on('end', () => {
+        console.log('disconnected from server');
+    });
+    }else{
+        process.stdout.write('please input you name\r\n')
+    }
+});
+
---
+++ server.js
@@ -0,0 +1,101 @@
+const net  = require('net');
+
+const DealInfo = {
+    clients:{},
+    login (receive, socket) {
+        console.log(`${receive.name}>${receive.message}`);
+        this.clients[receive.name] = socket;
+        for(let key in this.clients){
+            console.log(key, receive.name)
+            if(key != receive.name){
+                this.clients[key].write(JSON.stringify(receive));
+            }
+        }
+    },
+    server (receive, socket) {
+        for(let key in this.clients){
+            console.log(key, receive.name)
+            if(key != receive.name){
+                this.clients[key].write(JSON.stringify(receive));
+            }
+        }
+    },
+    client (receive, socket) {
+        for(let key in this.clients){
+            if(key == receive.to){
+                socket.write(JSON.stringify(receive));
+            }
+        }
+    }
+}
+//创建服务端
+const server = net.createServer((socket) => {
+
+    //获取当前连接的用户数
+    // server.getConnections((err, count) => {
+    //     if(err){
+    //         throw err;
+    //     }
+    //     console.log('client connect ' + count);
+    // })
+    //服务器信息：prot, host, family(IPv4)
+    // console.log(socket.address())
+    //客户端的ip, family, port, 这里的端口是分配给客户端的端口，随机分配。
+    console.log(socket.remoteAddress, socket.remoteFamily, socket.remotePort);
+    //暂停socket.pause(),重新开始socket.resume();
+    //发送消息成功返回true，失败false
+    // let returnInfo = socket.write('welcome come to here\r\n', 'utf8', () => {
+        // console.log('send:'+data)
+        // console.log(returnInfo)
+    // });
+    // socket.pipe(socket);
+
+    socket.on('data', (data) => {
+        let receive = JSON.parse(data.toString().trim());
+        switch(receive.to){
+            case "login":
+                DealInfo.login(receive, socket);
+                break;
+            case "server":
+                DealInfo.server(receive, socket);
+                break;
+            default:
+                DealInfo.client(receive, socket);
+                break;
+        }
+        
+        
+
+    }).on('error', (err) => {
+        var deleteKey;
+        for (var username in DealInfo.clients) {
+            if (DealInfo.clients.hasOwnProperty(username)) {
+                var client = DealInfo.clients[username];
+                if (socket === client) {
+                    deleteKey = username;
+                }
+            }
+        }
+        delete DealInfo.clients[deleteKey];
+        // DealInfo.clients.splice(DealInfo.clients.indexOf(socket), 1);
+        server.getConnections((err, count) => {
+            if(err){
+                throw err;
+            }
+            console.log(`${deleteKey}下线了 当前在线人数${count}`);
+        })
+        
+    })
+
+    socket.on('end', () => {
+        console.log('client disconnected');
+    })
+}).on('error', (err) => {
+    throw err;
+})
+server.listen({
+    port:4433,
+    host:'172.20.131.140'
+}, () => {
+    console.log('server bound')
+})
